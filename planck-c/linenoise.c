4bc03199c082d93356686c038b67037c6cd7b20b
Mike Fikes <mike@fikesfarm.com>
2017-01-30 20:11:24 -0500

    cleanup

--- planck-c/linenoise.c
+++ planck-c/linenoise.c
@@ -773,8 +773,7 @@ int isPasting() {
     return pasting;
 }
 
-static void set_current(struct linenoiseState *current, const char *str)
-{
+static void set_current(struct linenoiseState *current, const char *str) {
     strncpy(current->buf, str, current->buflen);
     current->buf[current->buflen - 1] = 0;
     current->len = current->pos = strlen(current->buf);
@@ -942,107 +941,106 @@ static int linenoiseEdit(int stdin_fd, int stdout_fd, char *buf, size_t buflen, 
         } else if (c == keymap[KM_HISTORY_NEXT]) {
             linenoiseEditHistoryNext(&l, LINENOISE_HISTORY_NEXT);
         } else if (c == keymap[KM_REVERSE_I_SEARCH]) {
-            {
-                /* Display the reverse-i-search prompt and process chars */
-                char rbuf[50];
-                char rprompt[80];
-                int rchars = 0;
-                int rlen = 0;
-                int searchpos = history_len - 1;
 
-                rbuf[0] = 0;
-                while (1) {
-                    int n = 0;
-                    const char *p = NULL;
-                    int skipsame = 0;
-                    int searchdir = -1;
+            /* Display the reverse-i-search prompt and process chars */
+            char rbuf[50];
+            char rprompt[80];
+            int rchars = 0;
+            int rlen = 0;
+            int searchpos = history_len - 1;
 
-                    snprintf(rprompt, sizeof(rprompt), "(reverse-i-search)`%s': ", rbuf);
-                    l.prompt = rprompt;
-                    refreshLine(&l);
-                    //was: c = fd_read(current);
-                    nread = read(l.ifd, &c, 1);
-                    // TODO check nread
-                    if (/*c == ctrl('H') || */c == keymap[KM_DELETE]) {
-                        if (rchars) {
-                            // was: int p = utf8_index(rbuf, --rchars);
-                            int p = --rchars;
-                            rbuf[p] = 0;
-                            rlen = strlen(rbuf);
-                        }
+            rbuf[0] = 0;
+            while (1) {
+                int n = 0;
+                const char *p = NULL;
+                int skipsame = 0;
+                int searchdir = -1;
+
+                snprintf(rprompt, sizeof(rprompt), "(reverse-i-search)`%s': ", rbuf);
+                l.prompt = rprompt;
+                refreshLine(&l);
+                //was: c = fd_read(current);
+                nread = read(l.ifd, &c, 1);
+                // TODO check nread
+                if (/*c == ctrl('H') || */c == keymap[KM_DELETE]) {
+                    if (rchars) {
+                        // was: int p = utf8_index(rbuf, --rchars);
+                        int p = --rchars;
+                        rbuf[p] = 0;
+                        rlen = strlen(rbuf);
+                    }
+                    continue;
+                }
+
+                if (c == keymap[KM_HISTORY_PREVIOUS]) {
+                    /* Search for the previous (earlier) match */
+                    if (searchpos > 0) {
+                        searchpos--;
+                    }
+                    skipsame = 1;
+                } else if (c == keymap[KM_HISTORY_NEXT]) {
+                    /* Search for the next (later) match */
+                    if (searchpos < history_len) {
+                        searchpos++;
+                    }
+                    searchdir = 1;
+                    skipsame = 1;
+                } else if (c >= ' ') {
+                    if (rlen >= (int) sizeof(rbuf) + 3) {
                         continue;
                     }
 
-                    if (c == keymap[KM_HISTORY_PREVIOUS]) {
-                        /* Search for the previous (earlier) match */
-                        if (searchpos > 0) {
-                            searchpos--;
-                        }
-                        skipsame = 1;
-                    } else if (c == keymap[KM_HISTORY_NEXT]) {
-                        /* Search for the next (later) match */
-                        if (searchpos < history_len) {
-                            searchpos++;
-                        }
-                        searchdir = 1;
-                        skipsame = 1;
-                    } else if (c >= ' ') {
-                        if (rlen >= (int) sizeof(rbuf) + 3) {
+                    // was: n = utf8_getchars(rbuf + rlen, c);
+                    *(rbuf + rlen) = c;
+                    n = 1;
+                    rlen += n;
+                    rchars++;
+                    rbuf[rlen] = 0;
+
+                    /* Adding a new char resets the search location */
+                    searchpos = history_len - 1;
+                } else {
+                    /* Exit from incremental search mode */
+                    break;
+                }
+
+                /* Now search through the history for a match */
+                for (; searchpos >= 0 && searchpos < history_len; searchpos += searchdir) {
+                    p = strstr(history[searchpos], rbuf);
+                    if (p) {
+                        /* Found a match */
+                        if (skipsame && strcmp(history[searchpos], l.buf) == 0) {
+                            /* But it is identical, so skip it */
                             continue;
                         }
-
-                        // was: n = utf8_getchars(rbuf + rlen, c);
-                        *(rbuf + rlen) = c;
-                        n = 1;
-                        rlen += n;
-                        rchars++;
-                        rbuf[rlen] = 0;
-
-                        /* Adding a new char resets the search location */
-                        searchpos = history_len - 1;
-                    } else {
-                        /* Exit from incremental search mode */
+                        /* Copy the matching line and set the cursor position */
+                        set_current(&l, history[searchpos]);
+                        // was: current->pos = utf8_strlen(history[searchpos], p - history[searchpos]);
+                        l.pos = p - history[searchpos];
                         break;
                     }
-
-                    /* Now search through the history for a match */
-                    for (; searchpos >= 0 && searchpos < history_len; searchpos += searchdir) {
-                        p = strstr(history[searchpos], rbuf);
-                        if (p) {
-                            /* Found a match */
-                            if (skipsame && strcmp(history[searchpos], l.buf) == 0) {
-                                /* But it is identical, so skip it */
-                                continue;
-                            }
-                            /* Copy the matching line and set the cursor position */
-                            set_current(&l, history[searchpos]);
-                            // was: current->pos = utf8_strlen(history[searchpos], p - history[searchpos]);
-                            l.pos = p - history[searchpos];
-                            break;
-                        }
-                    }
-                    if (!p && n) {
-                        /* No match, so don't add it */
-                        rchars--;
-                        rlen -= n;
-                        rbuf[rlen] = 0;
-                    }
                 }
-                if (c == keymap[KM_CANCEL_SEARCH]) {
-                    /* terminates the search with no effect */
-                    set_current(&l, "");
-                    l.prompt = prompt;
-                    c = 0;
-                } else if (c == keymap[KM_FINISH_SEARCH] || c == keymap[KM_ESC]) {
-                    /* terminates the search leaving the buffer in place */
-                    l.prompt = prompt;
-                    c = 0;
+                if (!p && n) {
+                    /* No match, so don't add it */
+                    rchars--;
+                    rlen -= n;
+                    rbuf[rlen] = 0;
                 }
-                /* Go process the char normally */
-                refreshLine(&l);
-                goto process_char;
             }
-            break;
+            if (c == keymap[KM_CANCEL_SEARCH]) {
+                /* terminates the search with no effect */
+                set_current(&l, "");
+                l.prompt = prompt;
+                c = 0;
+            } else if (c == keymap[KM_FINISH_SEARCH] || c == keymap[KM_ESC]) {
+                /* terminates the search leaving the buffer in place */
+                l.prompt = prompt;
+                c = 0;
+            }
+            /* Go process the char normally */
+            refreshLine(&l);
+            goto process_char;
+
         } else if (c == keymap[KM_ESC]) {    /* escape sequence */
             /* Read the next two bytes representing the escape sequence.
              * Use two calls to handle slow terminals returning the two
